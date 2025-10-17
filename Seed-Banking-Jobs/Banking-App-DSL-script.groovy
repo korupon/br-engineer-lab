5bf2c0e28b546c3dc406fe3e2e658e89f340d8bf
NaveenSoft <aws53895@gmail.com>
2025-10-17 20:07:49 +0530

    Seed-Banking-Jobs

---
+++ Seed-Banking-Jobs/Banking-App-DSL-script.groovy
@@ -0,0 +1,29 @@
+folder('Banking-App') {
+    description('Folder for Banking Application Pipelines')
+}
+
+def environments = ['SIT', 'UAT', 'PROD']
+
+environments.each { env ->
+    pipelineJob("Banking-App/Pipeline-${env}") {
+        description("Pipeline for ${env} environment of Banking App")
+
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url('https://github.com/<your-username>/Banking-App-CI-CD.git')
+                        }
+                        branch('main')
+                    }
+                }
+                scriptPath("Seed-Banking-Jobs/Jenkinsfile-Pipeline-${env}")
+            }
+        }
+
+        triggers {
+            scm('H/5 * * * *')  // Optional: poll every 5 mins
+        }
+    }
+}
